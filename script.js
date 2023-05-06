eeabeb2040fef1115d2b5d34cca328e77ae92a54
Lucas Woibau <lucaswoibau@hotmail.com>
2023-05-06 14:19:38 -0300

    implementando o bonus de 8 lados

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-let borda = document.getElementById('borda')
-let borderValue = 0;
+const borda = document.getElementById('borda')
+const inputs = document.querySelectorAll('input[type = "number"]')
 
-borda.oninput = function(){
-    borderValue = document.getElementsByClassName('left1')[0].value;
-    borda.style.borderTopLeftRadius = `${borderValue}`;
+inputs.forEach(input => {
+    input.addEventListener('input', () =>{
+        const topLeft = document.getElementById('topLeft').value;
+        const bottomLeft = document.getElementById('bottomLeft').value;
+        const topRight = document.getElementById('topRight').value;
+        const bottomRight = document.getElementById('bottomRight').value;
+
+        borda.style.borderTopLeftRadius = `${topLeft}px`;
+        borda.style.borderBottomLeftRadius = `${bottomLeft}px`;
+        borda.style.borderTopRightRadius= `${topRight}px`;
+        borda.style.borderBottomRightRadius = `${bottomRight}px`
+    })
+});
+
+function CopiarValores(){
+    const input = document.getElementById('botao-copiar')
+
+    topLeft = document.getElementById('topLeft').value;
+    bottomLeft = document.getElementById('bottomLeft').value;
+    topRight = document.getElementById('topRight').value;
+    bottomRight = document.getElementById('bottomRight').value;
+
+    const textoClipboard = `${topLeft}px ${bottomLeft}px ${topRight}px ${bottomRight}px`
+
+    navigator.clipboard.writeText(textoClipboard.valueOf())
+        
+    .then(() => {
+        alert("Texto copiado para área de transferência!")
+    })
+    .catch((error) =>{
+        console.error("Erro ao copiar o texto: ",error)
+    })
 }
